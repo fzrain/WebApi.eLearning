8aa599721aed853da1602924c9e42a48ac088e13
fzrain <250970574@qq.com>
2014-08-19 17:03:08 +0800

    Odata只读服务

---
+++ Learning.ODataService/App_Start/WebApiConfig.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.OData.Builder;
+using Learning.Data.Entities;
+using Microsoft.Data.Edm;
+
+namespace Learning.ODataService
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API 配置和服务
+
+            // Web API 路由
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            config.Routes.MapODataRoute("elearningOData", "OData", GenerateEdmModel());
+        }
+        private static IEdmModel GenerateEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Course>("Courses");
+            builder.EntitySet<Enrollment>("Enrollments");
+            builder.EntitySet<Subject>("Subjects");
+            builder.EntitySet<Tutor>("Tutors");
+            var tutorsEntitySet = builder.EntitySet<Tutor>("Tutors");
+
+            tutorsEntitySet.EntityType.Ignore(s => s.UserName);
+            tutorsEntitySet.EntityType.Ignore(s => s.Password);
+           
+            return builder.GetEdmModel();
+        }
+    }
+}
---
+++ Learning.ODataService/Controllers/CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.OData;
+using Learning.Data;
+using Learning.Data.Entities;
+
+namespace Learning.ODataService.Controllers
+{
+    public class CoursesController : EntitySetController<Course, int>
+    {
+        LearningContext ctx = new LearningContext();
+
+        [Queryable(PageSize = 10)]
+        public override IQueryable<Course> Get()
+        {
+            return ctx.Courses.AsQueryable();
+        }
+
+        protected override Course GetEntityByKey(int key)
+        {
+            return ctx.Courses.Find(key);
+        }
+    }
+}
